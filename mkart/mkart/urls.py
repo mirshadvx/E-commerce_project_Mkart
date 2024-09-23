f345df85facc0a13014fafac1e476f41ed52ead0
mirshad <mirshadkvx@gmail.com>
2024-09-23 09:44:12 -0700

    13.1

--- mkart/mkart/urls.py
+++ mkart/mkart/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+from home.views import custom_404, custom_500
 
+handler404 = custom_404
+handler500 = custom_500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,9 +19,3 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += path('__debug__/',include(debug_toolbar.urls)),
-    
-from django.conf.urls import handler404, handler500
-from home.views import custom_404, custom_500
-
-handler404 = custom_404
-handler500 = custom_500
