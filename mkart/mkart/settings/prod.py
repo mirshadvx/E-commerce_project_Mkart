0cfd56b6364de33c0cae9b73b2f49e3785123d34
mirshad <mirshadkvx@gmail.com>
2026-05-10 22:50:36 +0530

    fixingg

--- mkart/mkart/settings/prod.py
+++ mkart/mkart/settings/prod.py
@@ -14,8 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR.parent / "static",
 ]
 
 STORAGES = {
--- mkart/mkart/wsgi.py
+++ mkart/mkart/wsgi.py
@@ -6,12 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkart.settings')
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkart.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkart.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkart.mkart.settings.prod')
 
 application = get_wsgi_application()
