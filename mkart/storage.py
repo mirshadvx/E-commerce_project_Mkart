25fce1b6b3a3e18e276ac4481aa242d7d9acef93
mirshad <mirshadkvx@gmail.com>
2026-05-13 12:06:37 +0530

    testing with custom storage 3

--- mkart/storage.py
+++ mkart/storage.py
@@ -1,4 +1,11 @@
 from whitenoise.storage import CompressedManifestStaticFilesStorage
+from django.core.files.storage import get_storage_class
 
-class ForgivingManifestStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    manifest_strict = False  # Don't crash on missing referenced files
+class IgnoreMissingCompressedStorage(CompressedManifestStaticFilesStorage):
+    def post_process(self, *args, **kwargs):
+        try:
+            return super().post_process(*args, **kwargs)
+        except Exception as e:
+            if "could not be found" in str(e):
+                return  # Ignore missing source maps
+            raise
